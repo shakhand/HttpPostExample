e3a4875150eae12bb3d9a5cd9ff7984dee967045
shakhand <hurricane_200@163.com>
2016-06-19 18:48:51 +0800

    Allow user to specify parameters of request
    1. Adapt UI to enable user to specify URL and account
    2. Adapt Activity class to get the paramters specified by user

--- app/src/main/java/com/example/shakhand/httptest/PostHttpActivity.java
+++ app/src/main/java/com/example/shakhand/httptest/PostHttpActivity.java
@@ -6,6 +6,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import org.json.JSONException;
@@ -25,32 +26,42 @@ public class PostHttpActivity extends AppCompatActivity {
 
     public static final String TAG = "POST_HTTP";
     private TextView mDisplayView;
+    private EditText mUrlEditText;
+    private EditText mAccountEditText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_post_http);
 
-        mDisplayView = (TextView)findViewById(R.id.display);
-
+        mDisplayView       = (TextView)findViewById(R.id.display);
+        mUrlEditText       = (EditText)findViewById(R.id.urlEditText);
+        mAccountEditText  = (EditText)findViewById(R.id.accountEditText);
     }
 
     public void postHttpClick(View view) {
-        Log.i(TAG, "post it");
-        new HttpPostTask().execute(new Object());
+        String[] inputs = new String[2];
+        String url = mUrlEditText.getText().toString();
+        String account = mAccountEditText.getText().toString();
+        inputs[0] = url;
+        inputs[1] = account;
+
+        Log.i(TAG, "post " + url);
+        new HttpPostTask().execute(inputs);
     }
 
-    class HttpPostTask extends  AsyncTask<Object, Object, String>
+    class HttpPostTask extends  AsyncTask<String, Object, String>
     {
         @Override
-        protected String doInBackground(Object... params) {
+        protected String doInBackground(String... inputs) {
             String result = "";
             try {
-                URL url = new URL("http://192.168.95.1:7876/nxt");
+                URL url = new URL(inputs[0]);
+                String account = inputs[1];
                 HttpURLConnection urlCon = (HttpURLConnection) url.openConnection();
                 urlCon.setRequestMethod("POST");
                 urlCon.setDoInput(true);
                 urlCon.setDoOutput(true);
-                String paramsString = "requestType=getAccount&account=NXT-WGS8-B6T6-4ZJL-DPLQM";
+                String paramsString = "requestType=getAccount&account=" + account;
                 byte[] paramsBytes = paramsString.getBytes("utf8");
                 OutputStream outputStream = urlCon.getOutputStream();
                 outputStream.write(paramsBytes);
